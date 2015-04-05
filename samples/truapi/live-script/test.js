55f311162364c83772f8916ab5c9938b7a57637c
Yaron Naveh <yaronn01@gmail.com>
2015-04-05 18:16:52 +0300

    add a sample script

---
+++ samples/truapi/live-script/test.js
@@ -0,0 +1,24 @@
+var r = require('request')
+
+exports = module.exports = function (vuser) {
+
+  vuser.action('Flow', function (svc, done) {
+    
+    svc.transaction.start('map');
+
+    r("http://ec2-23-21-64-152.compute-1.amazonaws.com/?type=map", function(err, res, body) {
+      
+      if (res.statusCode != 200 || err) {
+        svc.logger.error("status code is: " + res.statusCode + ". err is: " + err);
+        svc.transaction.end('map', svc.transaction.FAIL);
+        done()
+        return
+      }
+      
+      svc.transaction.end('map', svc.transaction.PASS);
+      done()
+    })  
+
+  });
+
+};
